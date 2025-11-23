e55393424fb528e2f83c791d52fad204a51245a8
mailforbrawlstars1919-code <mailforbrawlstars1919@gmail.com>
2025-11-23 18:43:39 +0300

    Delete duyuru_gonder.php

--- duyuru_gonder.php
+++
@@ -1,9 +0,0 @@
-<?php
-$data = json_decode(file_get_contents('php://input'), true);
-if ($data) {
-    file_put_contents('duyuru.json', json_encode($data, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
-    echo 'Duyuru kaydedildi!';
-} else {
-    echo 'Veri alınamadı!';
-}
-?>
